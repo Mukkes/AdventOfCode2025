ee50f1cdab7adc29698855ca993856dd31b38de3
Marcel Zwart <marcelzwart92@gmail.com>
2025-12-12 14:02:41 +0100

    Day 3 part 2.

--- Day03/Day03.c
+++ Day03/Day03.c
@@ -9,40 +9,24 @@ int main()
     FILE *filePointer;
     filePointer = fopen("Input.txt", "r");
     int length = 100;
-    char line[length + 5];
-    int resultPart1 = 0;
-    int resultPart2 = 0;
+    char line[length + 2];
+    long long resultPart1 = 0;
+    long long resultPart2 = 0;
     if (filePointer != NULL)
     {
         while (fgets(line, sizeof(line), filePointer))
         {
-            int maxJoltage = 11;
-            for (int i = 0; i < length - 1; i++)
-            {
-                int number = line[i] - '0';
-                printf("i: %d\n", i);
-                printf("line[i]: %d\n", line[i]);
-                printf("number: %d\n", number);
-                int joltage = (number * 10) + 1;
-                if (joltage > maxJoltage)
-                {
-                    maxJoltage = joltage;
-                }
-                number = line[i + 1] - '0';
-                joltage = ((maxJoltage / 10) * 10) + number;
-                if (joltage > maxJoltage)
-                {
-                    maxJoltage = joltage;
-                }
-            }
-            printf("maxJoltage: %d\n", maxJoltage);
+            line[strcspn(line, "\n")] = 0;
+            long long maxJoltage = GetMaxJoltage(line, 2);
             resultPart1 += maxJoltage;
+            maxJoltage = GetMaxJoltage(line, 12);
+            resultPart2 += maxJoltage;
         }
     }
     fclose(filePointer);
 
-    printf("Part1: %d\n", resultPart1);
-    printf("Part2: %d\n", resultPart2);
+    printf("Part1: %lld\n", resultPart1);
+    printf("Part2: %lld\n", resultPart2);
 
     return 0;
 }
@@ -50,24 +34,41 @@ int main()
 long long GetMaxJoltage(char *batteries, int size)
 {
     char maxJoltage[size + 1];
-    maxJoltage[size] = '\0';
+    maxJoltage[size] = 0;
+    // Fill maxJoltage with 1s.
     for (int i = 0; i < size; i++)
     {
-        maxJoltage[i] = 1;
+        maxJoltage[i] = '1';
     }
 
+    // Loop through batteries.
     int length = strlen(batteries);
     for (int i = 0; i < length; i++)
     {
-        for (int j = 0; j < size; j++)
+        // Set start next loop for when highest value is at end of array.
+        int start = 0;
+        if (i + size > length)
+        {
+            start = i + size - length;
+        }
+        // Loop through maxJoltage.
+        for (int j = start; j < size; j++)
         {
             char newMaxJoltage[size + 1];
-            newMaxJoltage[size] = '\0';
+            newMaxJoltage[size] = 0;
             strcpy(newMaxJoltage, maxJoltage);
             newMaxJoltage[j] = batteries[i];
+            // Determine if maxJoltage with batterie[i] on maxJoltage[j] is larger.
             if (atoll(newMaxJoltage) > atoll(maxJoltage))
             {
+                // Update maxJoltage.
                 strcpy(maxJoltage, newMaxJoltage);
+                // Fill rest maxJoltage with 1s.
+                for (int k = j + 1; k < size; k++)
+                {
+                    maxJoltage[k] = '1';
+                }
+                break;
             }
         }
     }
