8da2d7307db15bfbbe0ae7e8c8c66a11dbcd2b4a
Marcel Zwart <marcelzwart92@gmail.com>
2026-04-22 15:45:02 +0200

    Day 5 part 2

--- Day05/Day05.c
+++ Day05/Day05.c
@@ -21,6 +21,74 @@ bool InsideRange(Range *ranges, size_t ingredientId, size_t rangesCount)
     return false;
 }
 
+bool DoesRangeOverlap(Range *rangeA, Range *rangeB)
+{
+    if ((rangeB->Start >= rangeA->Start) && (rangeB->Start <= rangeA->End))
+    {
+        return true;
+    }
+    if ((rangeB->End >= rangeA->Start) && (rangeB->End <= rangeA->End))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool DoRangesOverlap(Range *rangeA, Range *rangeB)
+{
+    if (DoesRangeOverlap(rangeA, rangeB))
+    {
+        return true;
+    }
+    if (DoesRangeOverlap(rangeB, rangeA))
+    {
+        return true;
+    }
+    return false;
+}
+
+Range MergeRanges(Range *rangeA, Range *rangeB)
+{
+    Range result;
+    if (rangeA->Start < rangeB->Start)
+    {
+        result.Start = rangeA->Start;
+    }
+    else
+    {
+        result.Start = rangeB->Start;
+    }
+    if (rangeA->End > rangeB->End)
+    {
+        result.End = rangeA->End;
+    }
+    else
+    {
+        result.End = rangeB->End;
+    }
+    return result;
+}
+
+void SortRanges(Range *ranges, size_t count)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t j = 1; j < count; j++)
+        {
+            if (ranges[j - 1].Start > ranges[j].Start)
+            {
+                Range tmp;
+                tmp.Start = ranges[j - 1].Start;
+                tmp.End = ranges[j - 1].End;
+                ranges[j - 1].Start = ranges[j].Start;
+                ranges[j - 1].End = ranges[j].End;
+                ranges[j].Start = tmp.Start;
+                ranges[j].End = tmp.End;
+            }
+        }
+    }
+}
+
 int main()
 {
     char *fileName = "Input.txt";
@@ -88,6 +156,53 @@ int main()
     }
 
     printf("part 1: %zu\n", freshCount);
+
+    SortRanges(ranges, rangesCount);
+
+    Range *mergedRanges = malloc(200 * sizeof(Range));
+    if (mergedRanges == NULL)
+    {
+        return 1;
+    }
+
+    size_t mergedRangesCount = 0;
+    for (size_t i = 0; i < rangesCount; i++)
+    {
+        bool overlap = false;
+        for (size_t j = 0; j < mergedRangesCount; j++)
+        {
+            if (DoRangesOverlap(&ranges[i], &mergedRanges[j]))
+            {
+                Range mergedRange = MergeRanges(&ranges[i], &mergedRanges[j]);
+                mergedRanges[j].Start = mergedRange.Start;
+                mergedRanges[j].End = mergedRange.End;
+                printf("m mergedrange[%zu]: start: %zu end: %zu\n", j,
+                       mergedRanges[j].Start, mergedRanges[j].End);
+                overlap = true;
+            }
+        }
+        if (!overlap)
+        {
+            mergedRanges[mergedRangesCount].Start = ranges[i].Start;
+            mergedRanges[mergedRangesCount].End = ranges[i].End;
+            printf("a mergedrange[%zu]: start: %zu end: %zu\n",
+                   mergedRangesCount, mergedRanges[mergedRangesCount].Start,
+                   mergedRanges[mergedRangesCount].End);
+            mergedRangesCount++;
+        }
+    }
+
+    size_t totalFresh = 0;
+    for (size_t i = 0; i < mergedRangesCount; i++)
+    {
+        totalFresh += mergedRanges[i].End - mergedRanges[i].Start + 1;
+        printf("mergedrange[%zu]: start: %zu end: %zu\n", i,
+               mergedRanges[i].Start, mergedRanges[i].End);
+    }
+
+    printf("mergedRangesCount: %zu\n", mergedRangesCount);
+
+    printf("part 2: %zu\n", totalFresh);
 
     return 0;
 }
